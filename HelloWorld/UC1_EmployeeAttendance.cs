0615d2b2c5d502f12a7158d8012757cd0d50667c
Pankaj Kumar <pk110495@gmail.com>
2022-01-04 10:58:25 +0530

    code changes

---
+++ HelloWorld/UC1_EmployeeAttendance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace HelloWorld
+{
+	public class UC1_EmployeeAttendance
+	{
+		
+            public static void GetAttendance()
+            {
+                int IS_FULL_TIME = 1;
+                Random random = new Random();
+                int empCheck = random.Next(0, 2);
+                if (empCheck == IS_FULL_TIME)
+                    Console.WriteLine("Employee is present");
+                else
+                    Console.WriteLine("Employee is absent");
+            }
+
+
+
+
+
+    }
+}
+
---
+++ HelloWorld/UCLast Final Solution/IComputeEmpWage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace HelloWorld.UCLastFinalSolution
+{
+    public interface IComputeEmpWage
+    {
+        void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMnth);
+        void computeEmpWage();
+
+        int getTotalWage(string company);
+    }
+
+}
+
