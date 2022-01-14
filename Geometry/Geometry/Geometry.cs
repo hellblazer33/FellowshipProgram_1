cfd72c832814af84d0cbaacb89b1dbd8b4db566d
Pankaj Kumar <pk110495@gmail.com>
2022-01-14 09:34:09 +0530

    added geometry line comparision program

---
+++ Geometry/Geometry/Geometry.cs
@@ -0,0 +1,12 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+double x1 = 5;
+double x2 = 6;
+double y1 = 5;
+double y2 = 6;
+double length;
+double lineLength;
+length =( Math.Pow((x2 - x1),2 )+ Math.Pow( (y2 - y1),2));
+lineLength = Math.Sqrt(length);
+Console.WriteLine(lineLength);
