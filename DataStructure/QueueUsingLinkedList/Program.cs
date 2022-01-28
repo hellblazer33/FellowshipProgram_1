c6b61597f5241131684891bba7c24145a63ad771
Pankaj Kumar <pk110495@gmail.com>
2022-01-28 12:23:12 +0530

    Regex programs

--- DataStructure/QueueUsingLinkedList/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello World!");
-LinkedListQueue linkedListQueue = new LinkedListQueue();
-linkedListQueue.Enqueue(56);
-linkedListQueue.Enqueue(30);
-linkedListQueue.Enqueue(70);
-linkedListQueue.Display();// this.top=this.top.next
-Console.ReadKey();
